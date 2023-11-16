f0dcd03c8123c3ae7f0401dccb018ffd53d8ee85
Zandzakje <DamianTomasila@hotmail.com>
2023-11-16 14:04:08 +0100

    Project directory fix

---
+++ src/test/java/umu/kwetter/person/PersonApplicationTests.java
@@ -0,0 +1,13 @@
+package umu.kwetter.person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
